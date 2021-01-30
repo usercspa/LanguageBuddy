d9b8d88eb9131aaf47029ae9cd3c33a34d48500d
usercspa <spacchbk@gmail.com>
2021-01-30 07:18:42 +0000

    1 file modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "language-buddy-88fc7.firebaseapp.com",
-  projectId: "language-buddy-88fc7",
-  storageBucket: "language-buddy-88fc7.appspot.com",
-  messagingSenderId: "338553915691",
-  appId: "1:338553915691:web:91334fd9e27accf5bd9d6e"
-};
+var config = {
+    apiKey: "API_KEY",
+    authDomain: "language-buddy-88fc7.firebaseapp.com",
+    databaseURL: "https://language-buddy-88fc7-default-rtdb.firebaseio.com/",
+    storageBucket: "language-buddy-88fc7.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
+
+exports.addMessage = functions.https.onCall((data, context) => {
+  // ...
+});
+const text = data.text;
+const name = context.auth.token.name || null;
+
+var addMessage = firebase.functions().httpsCallable('addMessage');
+addMessage({ text: messageText })
+  .then((result) => {
+    // Read result of the Cloud Function.
+    var sanitizedMessage = result.data.text;
+  });
+
+//handling errors
+var addMessage = firebase.functions().httpsCallable('addMessage');
+addMessage({ text: messageText })
+  .then((result) => {
+    // Read result of the Cloud Function.
+    var sanitizedMessage = result.data.text;
+  })
+  .catch((error) => {
+    // Getting the Error details.
+    var code = error.code;
+    var message = error.message;
+    var details = error.details;
+    // ...
+  });
