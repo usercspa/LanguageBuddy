4a17c6d307b23c37ecff71099f63ef06c22dd5dc
usercspa <spacchbk@gmail.com>
2021-01-30 07:00:53 +0000

    2 files modified, 2 added

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "language-buddy-88fc7.firebaseapp.com",
+  projectId: "language-buddy-88fc7",
+  storageBucket: "language-buddy-88fc7.appspot.com",
+  messagingSenderId: "338553915691",
+  appId: "1:338553915691:web:91334fd9e27accf5bd9d6e"
+};
